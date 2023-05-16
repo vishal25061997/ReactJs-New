d723d37fe0392fd97c4e19f5de34c0f2c857ca89
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-16 14:14:46 +0530

    2-Add-restaurant-summary

---
+++ Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MealsSummary.module.css";
+
+
+const MealsSummary = ()=>{
+    return <section className={classes.summary}>
+<h2>Delicious Food, Delivered To YOu</h2>
+<p> Choose youer favourite meal from our broad selection of available meaks and enjoy a delicious lunch or dinner at home.</p>
+<p> All our meals are cooked with high quality ingredients, just-in -time and all of course by experienced chefs!</p>
+    </section>
+}
+
+export default MealsSummary;
